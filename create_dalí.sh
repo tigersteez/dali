bccffb4636e205909ed21489f07524b2ff84c9d1
W Hayden Fuss <wifu1234@gmail.com>
2016-01-10 16:02:05 -0500

    Improved minifying script. Will need to remove some scripts from it once I separate the ballgame from dalí.

--- create_dalí.sh
+++ create_dalí.sh
@@ -1,3 +1,1 @@
-cat ./js/dalí.js ./js/input.js ./js/gameObject.js ./js/física/collider.js ./js/gráficos/spriteMap.js ./js/gráficos/renderer.js ./js/gráficos/animation.js ./js/audio.js ./js/resources.js ./js/room.js ./js/events.js ./js/física/physics.js ./js/ball.js ./js/proxyBot.js ./js/game.js > ./dalí.js;
-minify ./dalí.js;
-rm -rf ./dalí.js;
+minify --output ./dalí.min.js --no-comments ./js/dalí.js ./js/input.js ./js/gameObject.js ./js/física/collider.js ./js/gráficos/spriteMap.js ./js/gráficos/renderer.js ./js/gráficos/animation.js ./js/audio.js ./js/resources.js ./js/room.js ./js/events.js ./js/física/physics.js ./js/ball.js ./js/proxyBot.js ./js/game.js;
